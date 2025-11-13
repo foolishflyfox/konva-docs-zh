0196e6a50545a9a6e6b32bf4bba4bb25e91ec59e
foolishflyfox <fenghuabin1992@163.com>
2025-11-14 05:13:15 +0800

    feat: 自定义组件中使用的代码块添加行号

--- docs/components/utils/HighLighterCode.vue
+++ docs/components/utils/HighLighterCode.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, inject, watch } from "vue";
+import { ref, onMounted, inject, watch, computed } from "vue";
 import type { Highlighter } from "shiki";
 import { useData } from "vitepress";
 
@@ -19,6 +19,9 @@ const highlighter = inject<Highlighter>("highlighter");
 
 const codeHtml = ref("");
 const { isDark } = useData();
+const lineNumberBorderColor = computed(() =>
+  isDark.value ? "#555" : "#d8d8d8"
+);
 
 function updateCodeHtml(isDarkTheme: boolean) {
   if (highlighter) {
@@ -46,14 +49,33 @@ onMounted(async () => {
 </script>
 
 <style scoped>
-/* 让行高亮颜色与 VitePress 默认同色 */
-:deep(.highlighted) {
-  background: var(--vp-code-line-highlight-color);
-}
 :deep(pre.shiki.github-dark) {
   padding: 6px 10px;
 }
 :deep(pre.shiki.github-light) {
   padding: 6px 10px;
+}
+
+/* 设置行号显示 */
+:deep(pre) {
+  counter-reset: line;
+}
+
+:deep(.line) {
+  counter-increment: line;
+  position: relative;
+  padding-left: 3em;
+}
+
+:deep(.line::before) {
+  content: counter(line);
+  position: absolute;
+  left: 0;
+  width: 2em;
+  text-align: right;
+  color: #6e7681;
+  border-right: 1px solid v-bind(lineNumberBorderColor);
+  padding-right: 0.5em;
+  margin-right: 0.5em;
 }
 </style>
