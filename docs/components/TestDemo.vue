40e06991a1ca98545fd16acb8f2d1b975b96f119
foolishflyfox <fenghuabin1992@163.com>
2025-11-13 09:16:43 +0800

    feat: 1.在markdown中引入vue组件; 2.在vue组件中引入vitepress组件

---
+++ docs/components/TestDemo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <VPButton @click="count++">count is: {{ count }}</VPButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { VPButton } from "vitepress/theme";
+
+const count = ref(0);
+</script>
+
+<style scoped></style>
