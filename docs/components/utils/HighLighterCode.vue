73dd6ef04312ffbb67b97de1c0f5d702253439d5
foolishflyfox <fenghuabin1992@163.com>
2025-11-14 06:02:48 +0800

    feat: 调整自定义组件代码块背景色

--- docs/components/utils/HighLighterCode.vue
+++ docs/components/utils/HighLighterCode.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- 如果想保留行号、高亮行等能力，可自己拼 class -->
   <div>
-    <pre v-if="codeHtml" class="shiki" v-html="codeHtml" />
+    <div v-if="codeHtml" v-html="codeHtml" />
   </div>
 </template>
 
@@ -49,16 +49,10 @@ onMounted(async () => {
 </script>
 
 <style scoped>
-:deep(pre.shiki.github-dark) {
-  padding: 6px 10px;
-}
-:deep(pre.shiki.github-light) {
-  padding: 6px 10px;
-}
-
 /* 设置行号显示 */
 :deep(pre) {
   counter-reset: line;
+  background-color: var(--vp-code-block-bg) !important;
 }
 
 :deep(.line) {
