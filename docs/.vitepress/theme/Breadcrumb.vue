b1806e0d6bb10fc59ec2b15b72740cc3a86b620b
foolishflyfox <fenghuabin1992@163.com>
2025-11-15 14:03:49 +0800

    feat: 面包屑预留槽

---
+++ docs/.vitepress/theme/Breadcrumb.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>todo: 显示面包屑</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,9 +2,11 @@
 import DefaultTheme from "vitepress/theme";
 import { createHighlighter } from "shiki";
 import type { Theme } from "vitepress";
+import { h } from "vue";
+import Breadcrumb from "./Breadcrumb.vue";
 
 export default {
-  ...DefaultTheme,
+  extends: DefaultTheme,
   /**
    * 增强 Vue 实例
    */
@@ -14,8 +16,15 @@ export default {
       themes: ["github-dark", "github-light"],
       langs: ["js", "ts", "vue", "json", "html"],
     });
-
     // 注入到 Vue app 实例
     app.provide("highlighter", highlighter);
+  },
+  /**
+   * 自定义布局: 添加面包屑
+   */
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      "doc-before": () => h(Breadcrumb),
+    });
   },
 } satisfies Theme;
