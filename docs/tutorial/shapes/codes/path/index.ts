96b5ea2e4a874960de65ff63da00721899ca0d9a
foolishflyfox <fenghuabin1992@163.com>
2025-12-01 14:42:48 +0800

    feat: 完成 Path demo

---
+++ docs/tutorial/shapes/codes/path/index.ts
@@ -0,0 +1,74 @@
+import { createShapeCodesData } from "@docs/types";
+export * from "./demo";
+
+export const pathCodes = createShapeCodesData();
+pathCodes.vanilla.js = `import Konva from 'konva';
+
+const stage = new Konva.Stage({
+  container: 'container',
+  width: window.innerWidth,
+  height: window.innerHeight
+});
+
+const layer = new Konva.Layer();
+stage.add(layer);
+
+const path = new Konva.Path({
+  x: 50,
+  y: 50,
+  data: 'M12.582,9.551C3.251,16.237,0.921,29.021,7.08,38.564l-2.36,1.689l4.893,2.262l4.893,2.262l-0.568-5.36l-0.567-5.359l-2.365,1.694c-4.657-7.375-2.83-17.185,4.352-22.33c7.451-5.338,17.817-3.625,23.156,3.824c5.337,7.449,3.625,17.813-3.821,23.152l2.857,3.988c9.617-6.893,11.827-20.277,4.935-29.896C35.591,4.87,22.204,2.658,12.582,9.551z',
+  fill: 'green',
+  scale: {
+    x: 2,
+    y: 2
+  }
+});
+
+layer.add(path);`;
+
+pathCodes.react = `import { Stage, Layer, Path } from 'react-konva';
+
+const App = () => {
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Path
+          x={50}
+          y={50}
+          data="M12.582,9.551C3.251,16.237,0.921,29.021,7.08,38.564l-2.36,1.689l4.893,2.262l4.893,2.262l-0.568-5.36l-0.567-5.359l-2.365,1.694c-4.657-7.375-2.83-17.185,4.352-22.33c7.451-5.338,17.817-3.625,23.156,3.824c5.337,7.449,3.625,17.813-3.821,23.152l2.857,3.988c9.617-6.893,11.827-20.277,4.935-29.896C35.591,4.87,22.204,2.658,12.582,9.551z"
+          fill="green"
+          scaleX={2}
+          scaleY={2}
+        />
+      </Layer>
+    </Stage>
+  );
+};
+
+export default App;`;
+
+pathCodes.vue.app = `<template>
+  <v-stage :config="stageSize">
+    <v-layer>
+      <v-path :config="pathConfig" />
+    </v-layer>
+  </v-stage>
+</template>
+
+<script setup>
+const stageSize = {
+  width: window.innerWidth,
+  height: window.innerHeight
+};
+
+const pathConfig = {
+  x: 50,
+  y: 50,
+  data: 'M12.582,9.551C3.251,16.237,0.921,29.021,7.08,38.564l-2.36,1.689l4.893,2.262l4.893,2.262l-0.568-5.36l-0.567-5.359l-2.365,1.694c-4.657-7.375-2.83-17.185,4.352-22.33c7.451-5.338,17.817-3.625,23.156,3.824c5.337,7.449,3.625,17.813-3.821,23.152l2.857,3.988c9.617-6.893,11.827-20.277,4.935-29.896C35.591,4.87,22.204,2.658,12.582,9.551z',
+  fill: 'green',
+  scale: {
+    x: 2,
+    y: 2
+  }
+};
+</script>`;
--- docs/tutorial/shapes/path.md
+++ docs/tutorial/shapes/path.md
@@ -1,1 +1,13 @@
+<script setup>
+import { pathCodes, pathDemo } from './codes/path';
+</script>
+
 # 路径教程
+
+可以通过实例化 `Konva.Path` 对象，创建一个自定义路径图形。
+
+完整的属性和方法，请参考 [Path API](../../api/path)。
+
+<KShape :after-mounted="pathDemo" :width="200" :height="200" />
+
+<ShapeCode v-bind="pathCodes" />
