13e6e0d674776c48078f213b8b91f1a9fe1b8830
foolishflyfox <fenghuabin1992@163.com>
2026-01-07 17:45:28 +0800

    feat: 添加事件监听demo

---
+++ docs/tutorial/events/codes/listen-events/demo.ts
@@ -0,0 +1,52 @@
+import { createLayer } from "@docs/utils";
+import Konva from "konva";
+
+export function listenEventsDemo(stage: Konva.Stage) {
+  const layer = createLayer(stage);
+  const text = new Konva.Text({
+    x: 10,
+    y: 10,
+    fontFamily: "Calibri",
+    fontSize: 24,
+    text: "",
+    fill: "black",
+  });
+  layer.add(text);
+
+  const oval = new Konva.Ellipse({
+    x: stage.width() / 2,
+    y: stage.height() / 2,
+    radiusX: 100,
+    radiusY: 50,
+    fill: "yellow",
+    stroke: "black",
+    strokeWidth: 4,
+    listening: false,
+  });
+
+  oval.on("mouseover", function () {
+    writeMessage("Mouseover oval");
+  });
+  oval.on("mouseout", function () {
+    writeMessage("");
+  });
+
+  function writeMessage(message: string) {
+    text.text(message);
+  }
+
+  layer.add(oval);
+
+  // add button to toggle listening
+  const button = document.createElement("button");
+  button.classList.add("raw-style");
+  button.innerHTML = "开始监听";
+  // stag
+  button.addEventListener("click", () => {
+    const listening = !oval.listening();
+    oval.listening(listening);
+    button.innerHTML = listening ? "停止监听" : "开始监听";
+    layer.drawHit();
+  });
+  stage.container().prepend(button);
+}
--- docs/tutorial/events/listen-events.md
+++ docs/tutorial/events/listen-events.md
@@ -1,1 +1,11 @@
+<script setup>
+import { listenEventsDemo } from './codes/listen-events';
+</script>
+
 # 事件监听
+
+在使用 Konva 时，要监听或不监听事件，我们可以在实例化形状时将配置对象的 `listening` 属性设置为 `true` 或 `false，或者使用` `setListening()` 方法来设置 `listening` 属性。一旦我们为一个或多个节点设置了 `listening` 属性，还需要使用 `drawHit()` 方法为每个受影响的图层重绘命中图。
+
+操作说明：将鼠标悬停在椭圆上，观察事件处理程序未执行。点击"Listen"开始监听事件，观察事件处理程序现在已执行。
+
+<KShape :afterMounted="listenEventsDemo" :width="500" :height="300" />
