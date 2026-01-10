1b6ee85f4a9463821f01e4fbac4706384e897cf4
foolishflyfox <fenghuabin1992@163.com>
2026-01-10 09:22:32 +0800

    feat: 忽略描边demo

---
+++ docs/tutorial/select-transform/codes/ignore-stroke/index.ts
@@ -0,0 +1,67 @@
+import { createShapeCodesData } from "@docs/types";
+
+export * from "./demo";
+
+export const ignoreStrokeCodes = createShapeCodesData();
+ignoreStrokeCodes.vanilla.js = `import Konva from 'konva';
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const stage = new Konva.Stage({
+container: 'container',
+width: width,
+height: height,
+});
+
+const layer = new Konva.Layer();
+stage.add(layer);
+
+// first way - reset scale on transform end
+const rect1 = new Konva.Rect({
+x: 50,
+y: 50,
+width: 100,
+height: 100,
+fill: '#00ff00',
+stroke: 'black',
+strokeWidth: 5,
+draggable: true,
+});
+layer.add(rect1);
+
+const tr1 = new Konva.Transformer({
+nodes: [rect1],
+});
+layer.add(tr1);
+
+rect1.on('transformend', () => {
+// and increase width and height manually
+rect1.width(rect1.width() * rect1.scaleX());
+rect1.height(rect1.height() * rect1.scaleY());
+// after transform we need to reset scale
+rect1.scaleX(1);
+rect1.scaleY(1);
+});
+
+// second way - disable stroke scaling
+const rect2 = new Konva.Rect({
+x: 200,
+y: 50,
+width: 100,
+height: 100,
+fill: '#ff0000',
+stroke: 'black',
+strokeWidth: 5,
+draggable: true,
+strokeScaleEnabled: false,
+});
+layer.add(rect2);
+
+const tr2 = new Konva.Transformer({
+nodes: [rect2],
+ignoreStroke: true,
+});
+layer.add(tr2);
+
+`;
--- docs/tutorial/select-transform/ignore-stroke.md
+++ docs/tutorial/select-transform/ignore-stroke.md
@@ -1,1 +1,19 @@
+<script setup>
+import { ignoreStrokeDemo, ignoreStrokeCodes } from './codes/ignore-stroke'
+</script>
+
 # 忽略描边
+
+请记住，`Konva.Transformer` 会改变节点的 `scaleX` 和 `scaleY` 属性。默认情况下，当你变换一个形状时，其描边也会被缩放。在某些情况下，这不是理想的效果。
+
+有两种方法可以防止描边被缩放：
+
+重置形状的比例。
+
+组合使用 `shape.strokeScaleEnabled(false)` 和 `transformer.ignoreStroke(false)`。
+
+操作说明：这里有两个可以调整大小的矩形。绿色的矩形将重置其比例，红色的矩形则只是禁用了描边缩放。
+
+<KShape :afterMounted="ignoreStrokeDemo" :width="500" :height="300" />
+
+<ShapeCode v-bind="ignoreStrokeCodes" />
